c8f45432316a4ad3dde096848c426177a3d8b067
rezaramadhanirianto <rezaramadhanirianto@gmail.com>
2021-03-12 19:35:29 +0700

    Initial Commit

---
+++ WeaherApp/WeaherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeaherAppApp.swift
+//  WeaherApp
+//
+//  Created by Reza Ramadhan Irianto on 12/03/21.
+//
+
+import SwiftUI
+
+@main
+struct WeaherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
